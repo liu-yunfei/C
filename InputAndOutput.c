9be9062d92898859a5903a7a0be53ec5e9cbb373
liu-yunfei <jasonliuyf@gmail.com>
2020-12-22 22:56:10 +0800

    updated

--- InputAndOutput.c
+++ InputAndOutput.c
@@ -3,7 +3,7 @@ int main()
 {
     printf("Please enter your name:");
     char name[255];
-    scanf("%s",&name);
+    scanf("%[^\n]",&name);
     printf("Your name is %s\n",name);
     return 0;
 }
