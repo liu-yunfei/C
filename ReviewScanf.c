5783a0a43c6212f6db28ab58f590dd3dc23c64bc
liu-yunfei <jasonliuyf@gmail.com>
2020-12-28 19:58:44 +0800

    updated

--- ReviewScanf.c
+++ ReviewScanf.c
@@ -5,7 +5,7 @@ int main(void)
     char name[40];
 
     printf("Enter your name: ");
-    scanf("%s",&name); //Question: In C Primer, it said not use & when reading a string into a character array, but it is ok to use it here.
+    scanf("%s",&name); //Question: In C Primer Plus 6th, it said not use & when reading a string into a character array, but it is ok to use it here.
     printf("Enter your chosen number: ");
     scanf("%f",&number);
     printf("Hello %s, your number is %f.\n",name,number);
