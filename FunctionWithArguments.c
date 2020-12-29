c8b5e14f338f1a218fa536c99acc02fbb1678b6c
liu-yunfei <jasonliuyf@gmail.com>
2020-12-29 14:45:59 +0800

    updated

---
+++ FunctionWithArguments.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(void)
+{
+    int times;
+
+    printf("Please enter the times: ");
+    scanf("%d",&times);
+
+    ring(times);
+    return 0;
+}
+
+void ring(int number)
+{
+    while (--number>-1)
+    {
+        printf("Bell rings\a\n");
+    }
+}
