fdcc29e659853035b47e8a4253c58dc5c638951b
liu-yunfei <jasonliuyf@gmail.com>
2020-12-29 09:29:23 +0800

    updated

--- Loop.c
+++ Loop.c
@@ -1,4 +1,4 @@
-#include<stdio.h>
+#include <stdio.h>
 #define ADJUST 7.31
 int main(void)
 {
@@ -9,7 +9,7 @@ int main(void)
     while (shoe < 18.5)
     {
         foot = SCALE * shoe + ADJUST;
-        printf("%10.1f %15.2f inches\n",shoe,foot);
+        printf("%10.1f %15.2f inches\n", shoe, foot);
         shoe += 1.0;
     }
     printf("If the shoe fits, wear it.\n");
