5841e9a3bfd7e3f8465bd003af9ba5f1c5f97694
liu-yunfei <jasonliuyf@gmail.com>
2021-01-03 14:25:12 +0800

    updated

--- naive_exercise/NestedLoops2.c
+++ naive_exercise/NestedLoops2.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
-#define ROWS 6
-#define CHARS 10
 int main(void)
 {
+    const int ROWS = 6;
+    const int CHARS = 6;
     int row;
     char ch;
 
     for (row = 0; row < ROWS; row++)
     {
-        for (ch = 'A'; ch < ('A' + CHARS); ch++)
+        for (ch = ('A' + row); ch < ('A' + CHARS); ch++)
             printf("%c", ch);
         printf("\n");
     }
