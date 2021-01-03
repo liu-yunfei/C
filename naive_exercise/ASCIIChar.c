cf048dca57946105eb88c7a6b9d63a6ec4a46f17
liu-yunfei <jasonliuyf@gmail.com>
2021-01-03 11:32:23 +0800

    updated

---
+++ naive_exercise/ASCIIChar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(void)
+{
+    char ch;
+
+    for (ch = 'A'; ch <= 'Z'; ch++)
+    {
+        printf("The ASCII value for %c is %d.\n",ch,ch);
+    }
+    for (ch = "a"; ch <= 'z'; ch++)
+    {
+        printf("The ASCII value for %c is %d.\n",ch,ch);
+    }
+
+    return 0;
+}
